455810e0602594a0e0aaf7c191f791ea2f935225
Justin Li <your_email@example.com>
2021-11-16 15:07:14 -0800

    added button on main page

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,19 @@ export default function Home() {
     <div>
       <Navbar/>
       <div className={styles.landing}>
-        
+        <div className={styles.info}>
+          <h1 className={styles.heading}>I'm Justin Li.</h1>
+          <p className={styles.bodytext}>
+              I'm a developer <br/>
+              focused on design <br/>
+              and functionality.
+          </p>
+          <button className={styles.projectbutton}>
+          VIEW WORK
+        </button>
+        </div>
+        <div className={styles.graphic}>
+        </div>
       </div>
     </div>
   )
