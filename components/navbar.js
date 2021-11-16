0e66cf8f98b53dbb571cbcf42ffab83d9ae6826b
Justin Li <your_email@example.com>
2021-11-16 03:46:01 -0800

    added navbar and homescreen

--- components/navbar.js
+++ components/navbar.js
@@ -2,6 +2,19 @@ import styles from '../styles/Navbar.module.css';
 
 export default function Navbar() {
     return (
-      <div className={styles.navbar}></div>
+      <div className={styles.navbar}>
+        <img className={styles.logo} src="/finallogo.svg"/>
+        <ul className={styles.buttons}>
+          <li className={styles.link}>
+            PROJECTS
+          </li>
+          <li className={styles.link}>
+            CONTACT
+          </li>
+          <li className={styles.link}>
+            ABOUT
+          </li>
+        </ul>
+      </div>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function Home() {
   return (
     <div>
       <Navbar/>
+      <div className={styles.landing}>
+        
+      </div>
     </div>
   )
 }
