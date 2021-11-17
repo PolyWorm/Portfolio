4cee176a58c32e7b6624109deba377d11925972b
Justin Li <your_email@example.com>
2021-11-17 00:05:04 -0800

    added different favicon sizes

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,10 @@ export default function Home() {
     <div>
       <Head>
         <title>Justin Li here.</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
       </Head>
       <Navbar/>
         <div className={styles.landing}>
